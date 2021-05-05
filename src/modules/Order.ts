25290a0872a85e43d14a1800eed1b5e87782423b
Vladislav Savin <alienlilo001@gmail.com>
2021-05-05 20:33:47 +0800

    hot fix

--- src/modules/Order.ts
+++ src/modules/Order.ts
@@ -6,9 +6,9 @@ export const CANCELLED = 'cancelled'
 
 export function getAddressByNetwork(network: string): Address {
   if (network == 'mainnet') {
-    return Address.fromString('0xd412054cca18a61278ced6f674a526a6940ebd84')
-  } if (network == 'rinkeby') {
-    return Address.fromString('0xd412054cca18a61278ced6f674a526a6940ebd84')
+    return Address.fromString('0xC632734C623659916694126a2793d8EC11485137')
+  } if (network == 'kovan') {
+    return Address.fromString('0xC632734C623659916694126a2793d8EC11485137')
   }
 
   throw 'No Address specified'
