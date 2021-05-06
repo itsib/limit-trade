7ba7df76dca135a4fe790d82e0a3ab9dc6af8a94
Vladislav Savin <alienlilo001@gmail.com>
2021-05-07 01:19:11 +0800

    init mainnet

--- src/modules/Order.ts
+++ src/modules/Order.ts
@@ -6,7 +6,7 @@ export const CANCELLED = 'cancelled'
 
 export function getAddressByNetwork(network: string): Address {
   if (network == 'mainnet') {
-    return Address.fromString('0xC632734C623659916694126a2793d8EC11485137')
+    return Address.fromString('0x16A893F27FDB30CC05D8FD7a333a5AB841461658')
   } if (network == 'kovan') {
     return Address.fromString('0xC632734C623659916694126a2793d8EC11485137')
   }
